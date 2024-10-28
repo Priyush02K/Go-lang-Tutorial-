074617b603fa46cccf10948691b0987dbe50118c
Priyush02K <collegeinfopk02@gmail.com>
2024-10-28 14:05:47 +0530

    variable 02

---
+++ 03Variables/main.go
@@ -0,0 +1,22 @@
+//Variable Declaration With Initial Value
+package main
+import ("fmt")
+
+func main()  {
+	var student string="Priyush"
+	x := 2
+	fmt.Println(student)
+	fmt.Println(x)
+}
+
+//Variable Declaration Without Initial Value
+func main1()  {
+ var a string
+ var b int
+ var c bool
+ 
+ 
+ fmt.Println(a)
+ fmt.Println(b)
+ fmt.Println(c)
+}
