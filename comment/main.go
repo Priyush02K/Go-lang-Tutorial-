693fe13a5ca60220377389e4b8228017834ff01c
Priyush02K <collegeinfopk02@gmail.com>
2024-10-28 13:39:22 +0530

    go intro

---
+++ comment/main.go
@@ -0,0 +1,11 @@
+package main
+import ("fmt")
+
+func main() {
+	  // This is a comment
+
+  fmt.Println("Hello World!")
+
+   /* The code below will print Hello World
+  to the screen, and it is amazing */
+}
